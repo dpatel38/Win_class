78b8435cc7a35e8e42af02a17dcf40e0831a5b1a
dpatel38 <69981731+dpatel38@users.noreply.github.com>
2021-02-24 14:31:08 -0500

    Learned JavaScript

---
+++ week2/javascript/challenge1.js
@@ -0,0 +1,17 @@
+//challenge1
+
+var cups = 1
+if(cups < 3){
+    console.log("Yes. I'll take another cup of coffee!");
+} else {
+    console.log("I think I'm okay for now.");
+}
+
+// challenge2
+
+let carOilChange = 100;
+if(carOilChange > 500){
+    console.log('Yes I think I need an oil change.');
+}else{
+    console.log('My car doesnt need an oil change yet.');
+}
---
+++ week2/javascript/index.js
@@ -0,0 +1,12 @@
+console.log('Hello World');
+
+var myAge= 38;
+console.log(myAge);
+
+let amountRaisedSoFar = 1000;
+
+let donation = prompt("How much would you like to donate?"); 
+
+amountRaisedSoFar = Number(donation) + amountRaisedSoFar;
+
+console.log("We have now raised: " + amountRaisedSoFar + "!");
