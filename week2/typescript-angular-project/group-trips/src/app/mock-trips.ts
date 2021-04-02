974967e1870f44f093419f1d3134453b78a0ff7b
dpatel38 <daminipatels@gmail.com>
2021-04-02 13:45:26 -0400

    Added functionality to our angular project

---
+++ week2/typescript-angular-project/group-trips/src/app/mock-trips.ts
@@ -0,0 +1,11 @@
+import { Trip } from './trip';
+
+export const TRIPS: Trip[] = [
+  {id: 1,  name: 'Shanta',location: '', description: '', image: './assets/shanta.png'},
+  { id: 2, name: 'Lydia',location: '', description: '', image: './assets/vintage-photo-lydia.png'},
+  { id: 3, name: 'Kate',location: '', description: '', image: './assets/vintage-photo-kate.png'},
+  { id: 4, name: 'Damini',location: '', description: '', image: './assets/vintage-photo-damini.png'},
+  {id: 5,  name: 'David',location: '', description: '', image: './assets/daveVintage.png'},
+  {id: 6,  name: 'Diana',location: '', description: '', image: './assets/vintage-photo-diana.png'},
+  
+];
