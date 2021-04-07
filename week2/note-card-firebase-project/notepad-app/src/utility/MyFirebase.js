59866b0a6229f1e54ba7dbcaa256da36a5f250c1
dpatel38 <daminipatels@gmail.com>
2021-04-07 07:48:10 -0400

    Remove something

--- week2/note-card-firebase-project/notepad-app/src/utility/MyFirebase.js
+++ week2/note-card-firebase-project/notepad-app/src/utility/MyFirebase.js
@@ -3,13 +3,7 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 const config = {
-    apiKey: "AIzaSyCpb1f4TErfdL0ExkFPZRv0C4_c7wRG3Pw",
-    authDomain: "note-card-project.firebaseapp.com",
-    databaseURL: "https://note-card-project-default-rtdb.firebaseio.com",
-    projectId: "note-card-project",
-    storageBucket: "note-card-project.appspot.com",
-    messagingSenderId: "42755239151",
-    appId: "1:42755239151:web:93f8bd4f46e85bbfeeff1a"
+    //your key
   };
 
 firebase.initializeApp(config);
